74e93a71b7c11b2987070567bbaca59d4ed58fbc
huqi <qi.hu@zuolin.com>
2019-10-17 19:43:28 +0800

    add MainTime and import hutool

--- src/main/java/com/huqi/qs/java8/main/MainTime.java
+++ src/main/java/com/huqi/qs/java8/main/MainTime.java
@@ -1,6 +1,10 @@
 package com.huqi.qs.java8.main;
 
+import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 /**
  * @author huqi 20190920
@@ -8,5 +12,12 @@ import java.time.Instant;
 public class MainTime {
     public static void main(String[] args) {
         System.out.println(Instant.now().toEpochMilli());
+
+        LocalDateTime start = LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
+        Timestamp timestamp = Timestamp.valueOf(start);
+        System.out.println(timestamp.toString());
+        LocalDateTime end = LocalDateTime.of(LocalDate.now(), LocalTime.MAX);
+        timestamp = Timestamp.valueOf(end);
+        System.out.println(timestamp.toString());
     }
 }
