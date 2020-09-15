8daedc9fdc3e299155bf814d14c32ef32e49c2bf
huqi@Q.S <2715612836@qq.com>
2020-09-15 22:52:09 +0800

    modify something

--- src/main/java/com/huqi/qs/javase/main/TestC.java
+++ src/main/java/com/huqi/qs/javase/main/TestC.java
@@ -4,10 +4,10 @@ public class TestC implements Test {
     @Override
     public void printA(){
         System.out.println("test A");
-        var var1 = "111";
+        /*var var1 = "111";
         System.out.println(var1.getClass());
         System.out.println(Integer.parseInt(var1) - 100);
-        System.out.println("".isBlank());
+        System.out.println("".isBlank());*/
     }
 
     public static void main(String[] args) {
